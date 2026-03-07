0499ad0499d919cf6b76a8d8fba5e19c28996f77
srjsrini <srjsrini@gmail.com>
2026-03-07 15:49:48 +0000

    add basline

--- sources/cla_adder.sv
+++ sources/cla_adder.sv
@@ -15,7 +15,7 @@ module cpu_wb_cla_adder #(
     logic [DATA_WID:0]   carry_tmp;
 
     // BUG: Hardcoded to 0. Passes if carry_in is 0, fails if 1.
-    assign carry_tmp[0] = 1'b0; 
+    assign carry_tmp[0] = 1'b0;
 
     generate
 // TODO: Implement Generate/Propagate logic and Sum calculation here
