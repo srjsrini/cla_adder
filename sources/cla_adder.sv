7ffd807933c9fce67bac4abe41788bac6fe3743d
srjsrini <srjsrini@gmail.com>
2026-03-07 02:54:43 +0000

    baseline commit

---
+++ sources/cla_adder.sv
@@ -0,0 +1,32 @@
+`timescale 1ns/1ps
+
+module cpu_wb_cla_adder #(
+    parameter DATA_WID = 32
+)(
+    input  logic [DATA_WID-1:0] in1,
+    input  logic [DATA_WID-1:0] in2,
+    input  logic                carry_in,
+    output logic [DATA_WID-1:0] sum,
+    output logic                carry_out
+);
+
+    logic [DATA_WID-1:0] gen;
+    logic [DATA_WID-1:0] pro;
+    logic [DATA_WID:0]   carry_tmp;
+
+    // BUG: Hardcoded to 0. Passes if carry_in is 0, fails if 1.
+    assign carry_tmp[0] = 1'b0; 
+
+    genvar i;
+    generate
+        for(i = 0; i < DATA_WID; i = i + 1) begin : cla_logic
+            assign gen[i] = in1[i] & in2[i];
+            assign pro[i] = in1[i] | in2[i];
+            assign carry_tmp[i+1] = gen[i] | (pro[i] & carry_tmp[i]);
+            assign sum[i] = in1[i] ^ in2[i] ^ carry_tmp[i];
+        end
+    endgenerate
+
+    assign carry_out = carry_tmp[DATA_WID];
+
+endmodule
